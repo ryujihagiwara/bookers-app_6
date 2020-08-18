4217a40e40917abd24c632fba8eedc3d9c5d255d
ryujihagiwara <dragon7103next@icloud.com>
2020-08-18 23:54:58 +0900

    [Add] ルーティング

--- config/routes.rb
+++ config/routes.rb
@@ -5,10 +5,15 @@ Rails.application.routes.draw do
 
   devise_for :users
 
+  post 'follow/:id' => 'relationships#follow', as: 'follow'
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow'
+
   resources :users, only: [:show,:index,:edit,:update]
   resources :books do
   	resource :favorites, only: [:create, :destroy]
   	resources :book_comments, only: [:create, :destroy]
   end
+
+
 
 end
