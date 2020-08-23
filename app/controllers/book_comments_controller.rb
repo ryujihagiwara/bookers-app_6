a776fbe871dfba76b0f2decd344a1bb8cf93e659
ryujihagiwara <dragon7103next@icloud.com>
2020-08-23 21:48:30 +0900

    [Add] コメント非同期削除

--- app/controllers/book_comments_controller.rb
+++ app/controllers/book_comments_controller.rb
@@ -10,6 +10,7 @@ class BookCommentsController < ApplicationController
 	end
 
 	def destroy
+	  @book = Book.find(params[:book_id])
 	  BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
 	end
 
